8d1e22208991f3c9c9adfc4574c3d34011f5e37f
yumin-jung <jungym887@naver.com>
2023-04-05 14:11:10 +0900

    feat: Add another test

--- test_triangle.py
+++ test_triangle.py
@@ -2,6 +2,9 @@ from triangle import triangle
 
 def test_invalid1():
     assert triangle(-1, 0, 1) == -1
-    
+
+def test_invalid1():
+    assert triangle(0, 0, 1) == -1
+
 def test_equilateral():
     assert triangle(3, 3, 3) == 1
